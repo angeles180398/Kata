275e92046daf6b703788e6b743fad57180f657d0
angeles980318 <angeles98g@hotmail.com>
2021-02-09 13:46:14 -0600

    modifiqueindex.js

--- app/index.js
+++ app/index.js
@@ -1,14 +1,10 @@
-const http = require('http');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  
-});
+express()
+  .use(express.static(path.join(dirname, 'public')))
+  .set('views', path.join(dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
